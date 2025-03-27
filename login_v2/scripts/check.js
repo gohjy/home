433ee3423efbcb5f8368c743b798fab37542e37a
gohjy <154421097+gohjy@users.noreply.github.com>
2025-03-27 14:47:15 +0800

    Update check.js login links

--- login_v2/scripts/check.js
+++ login_v2/scripts/check.js
@@ -1,15 +1,15 @@
 try {
 	let userObj = JSON.parse(localStorage.getItem("userData-loginv2"));
 	// alert(userObj);
-	if ((userObj === null) && (location.pathname !== "/home/assets/error/not-logged-in.html")) {
-		let newURL = new URL("/home/assets/error/not-logged-in.html", location.origin);
+	if ((userObj === null) && (location.pathname !== "/nushweb/assets/error/not-logged-in.html")) {
+		let newURL = new URL("/nushweb/assets/error/not-logged-in.html", location.origin);
 		newURL.searchParams.append("to", location.pathname);
 		location.href = newURL;
 	} else {
 		if (userObj.restrictedAccess === true) {
 			let allowed = userObj.allowed;
 			if (!(allowed.includes(location.pathname + location.search + location.hash))) {
-				location.assign("/home/assets/error/no-access-right.html");
+				location.assign("/nushweb/assets/error/no-access-right.html");
 			}
 		} else {
             if (document.body) {
@@ -18,6 +18,6 @@ try {
 		}
 	}
 } catch(e) {
-	location.replace("/home/login_v2/public/login_page");
+	location.replace("/nushweb/login_v2/public/login_page");
 	alert("We ran into an internal error. Please login again.");
 }
